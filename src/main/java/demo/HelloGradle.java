55e0d0728d8ba8be1506bc42c23034de91a6e4d8
vikesathesh <38784371+vikesathesh@users.noreply.github.com>
2018-05-10 18:18:09 +0530

    commited

--- src/main/java/demo/HelloGradle.java
+++ src/main/java/demo/HelloGradle.java
@@ -1,12 +1,12 @@
 package demo;
 
-import org.joda.time.LocalTime;
+//import org.joda.time.LocalTime;
 class HelloGradle
 {
 public static void main(String args[])
 {
-LocalTime time = new LocalTime();
-System.out.println("Local time is :"+time);
+//LocalTime time = new LocalTime();
+//System.out.println("Local time is :"+time);
 System.out.println("Hello Gradle!");
 
 	
